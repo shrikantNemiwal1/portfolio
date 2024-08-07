30cd948906f1b69c2d73177a4aa157379e7d4d36
Shrikant Nemiwal <shrikant.nemiwal1@gmail.com>
2024-08-07 17:07:23 +0530

    update:content

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
+// const SplitText = require('./splitText');
+
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".projects", {
-  y: 300,
-  rotate: -10,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".image",
-    start: "top 75%",
-    end: "40% 30%",
-    ease: "power2.out",
-    // markers: true,
-  },
+gsap.set(".cursorFollower", { xPercent: -50, yPercent: -50 });
+gsap.set(".cursorDot", { xPercent: -50, yPercent: -50 });
+
+let xTo = gsap.quickTo(".cursorFollower", "x", {
+    duration: 0.6,
+    ease: "power5.out",
+  }),
+  yTo = gsap.quickTo(".cursorFollower", "y", {
+    duration: 0.6,
+    ease: "power5.out",
+  });
+let xToDot = gsap.quickTo(".cursorDot", "x", { duration: 0.001 }),
+  yToDot = gsap.quickTo(".cursorDot", "y", { duration: 0.001 });
+
+window.addEventListener("mousemove", (e) => {
+  xTo(e.clientX);
+  yTo(e.clientY);
+  xToDot(e.clientX);
+  yToDot(e.clientY);
 });
 
-gsap.from(".projects", {
-  x: 150,
-  scrollTrigger: {
-    trigger: ".image",
-    start: "top bottom",
-    end: "top top",
-    // markers: true,
-    scrub: 1,
-  },
-});
+// const childSplit = new SplitText(".hero-head", {
+//   type: "lines",
+//   linesClass: "aki_wrapper aki__word",
+// });
+// const parentSplit = new SplitText(".hero-head", {
+//   // type: "lines",
+//   linesClass: "aki_wrapper aki__line",
+// });
+// console.log(childSplit);
+// gsap.from(".aki__word", {
+//   duration: 2,
+//   yPercent: 150,
+//   ease: "power4",
+//   // stagger: 0.1,
+// });
+
+// gsap.from(".projects", {
+//   y: 300,
+//   rotate: -10,
+//   duration: 1,
+//   scrollTrigger: {
+//     trigger: ".image",
+//     start: "top 75%",
+//     end: "40% 30%",
+//     ease: "power2.out",
+//     // markers: true,
+//   },
+// });
+
+// gsap.from(".projects", {
+//   x: 150,
+//   scrollTrigger: {
+//     trigger: ".image",
+//     start: "top bottom",
+//     end: "top top",
+//     // markers: true,
+//     scrub: 1,
+//   },
+// });
 
 // gsap.from(".hero-main", {
 //   opacity: 0,
@@ -65,14 +104,14 @@ gsap.from(".projects", {
 //   }
 // );
 
-gsap.timeline().from(".hero-main .span", 1, {
-  y: 100,
-  ease: "power4.out",
-  delay: 1,
-  stagger: {
-    amount: 0.5,
-  },
-});
+// gsap.timeline().from(".hero-main .span", 1, {
+//   y: 100,
+//   ease: "power4.out",
+//   delay: 1,
+//   stagger: {
+//     amount: 0.5,
+//   },
+// });
 
 gsap
   .timeline({
@@ -89,83 +128,130 @@ gsap
     ease: "none",
   });
 
-gsap.from(".project-index", {
-  y: 200,
-  scrollTrigger: {
-    trigger: ".project-index",
-    start: "top bottom",
-    end: "top top",
-    // markers: true,
-    scrub: 1,
-  },
-});
+// gsap.from(".project-index", {
+//   y: 200,
+//   scrollTrigger: {
+//     trigger: ".project-index",
+//     start: "top bottom",
+//     end: "top top",
+//     // markers: true,
+//     scrub: 1,
+//   },
+// });
 
-gsap.from(".image", {
-  scrollTrigger: {
-    trigger: ".project-index",
-    start: "top bottom",
-    end: "top top",
-    // markers: true,
-    scrub: 1,
-  },
-});
+// gsap.from(".image", {
+//   scrollTrigger: {
+//     trigger: ".project-index",
+//     start: "top bottom",
+//     end: "top top",
+//     // markers: true,
+//     scrub: 1,
+//   },
+// });
 
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".image",
-      scrub: true,
-      start: "50% 100%",
-      end: "50% 50%",
-      // markers: true,
-    },
-  })
-  .to(".image", {
-    x: 100,
-    y: 30,
-    ease: "power5.in",
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".image",
+//       scrub: true,
+//       start: "50% 100%",
+//       end: "50% 50%",
+//       // markers: true,
+//     },
+//   })
+//   .to(".image", {
+//     x: 100,
+//     y: 30,
+//     ease: "power5.in",
+//   });
+
+const menuItems = document.querySelectorAll(".hero-texts li");
+document.getElementById("hero-texts").addEventListener("mouseenter", () => {
+  menuItems.forEach((item, index) => {
+    const hoverElement = item.querySelector(".menu-item-active");
+    const el = item.querySelector(".menu-item");
+    gsap.to(hoverElement, {
+      yPercent: -60,
+      duration: 0.5,
+      ease: "power5.in",
+      delay: index * 0.1,
+    });
+    gsap.to(el, {
+      yPercent: -60,
+      duration: 0.5,
+      ease: "power5.in",
+      delay: index * 0.1,
+    });
+  });
+});
+document.getElementById("hero-texts").addEventListener("mouseleave", () => {
+  menuItems.forEach((item, index) => {
+    const hoverElement = item.querySelector(".menu-item-active");
+    const el = item.querySelector(".menu-item");
+    gsap.to(hoverElement, {
+      yPercent: 100,
+      duration: 0.5,
+      ease: "power5.out",
+      delay: (menuItems.length - index - 1) * 0.1,
+    });
+    gsap.to(el, {
+      yPercent: 0,
+      duration: 0.5,
+      ease: "power5.out",
+      delay: (menuItems.length - index - 1) * 0.1,
+    });
+  });
+});
+// GSDevTools.create();
+
+const videos = document.querySelectorAll(".image video");
+
+videos.forEach((video) => {
+  video.addEventListener("mouseenter", () => {
+    video.play();
   });
 
-var xp = 0,
-  mouseX = 0;
-var yp = 0,
-  mouseY = 0;
-var xpDot = 0,
-  mouseX = 0;
-var ypDot = 0,
-  mouseY = 0;
-
-const cursorFollower = document.querySelector(".cursorFollower");
-const cursorFollowerDot = document.querySelector(".cursorFollowerDot");
-
-document.addEventListener("mousemove", (e) => {
-  mouseX = e.screenX;
-  mouseY = e.screenY;
+  video.addEventListener("mouseleave", () => {
+    video.pause();
+    video.currentTime = 0;
+  });
 });
 
-setInterval(function () {
-  xp += (mouseX - xp) / 15;
-  yp += (mouseY - yp) / 15;
-
-  cursorFollower.style.left = xp + "px";
-  cursorFollower.style.top = yp - 85 + "px";
-}, 10);
-
-setInterval(function () {
-  xpDot += (mouseX - xpDot) / 25;
-  ypDot += (mouseY - ypDot) / 25;
-
-  cursorFollowerDot.style.left = xpDot + "px";
-  cursorFollowerDot.style.top = ypDot - 85 + "px";
-}, 10);
-
 const themeBtn = document.getElementById("themeBtn");
+const cursorFollower = document.getElementById("cursorFollower");
+const cursorDot = document.getElementById("cursorDot");
 const themeProvider = document.getElementById("themeProvider");
+const heroSection = document.getElementById("hero-texts");
+
 themeBtn.addEventListener("click", () => {
   themeProvider.classList.toggle("dark");
   themeProvider.className.split(" ").includes("dark")
-    ? (cursorFollowerDot.innerText = "Light Mode")
-    : (cursorFollowerDot.innerText = "Dark Mode");
+    ? (cursorDot.innerText = "Light Mode")
+    : (cursorDot.innerText = "Dark Mode");
+});
+
+themeBtn.addEventListener("mouseenter", () => {
+  gsap.to(cursorDot, { scale: 12, duration: 0.3, ease: "power3.out" });
+  gsap.to(cursorFollower, { opacity: 0, scale: 0, duration: 0.3 });
+  themeProvider.className.split(" ").includes("dark")
+    ? (cursorDot.innerText = "Light Mode")
+    : (cursorDot.innerText = "Dark Mode");
+});
+
+themeBtn.addEventListener("mouseleave", () => {
+  gsap.to(cursorDot, { scale: 1, duration: 0.3, ease: "power3.out" });
+  gsap.to(cursorFollower, { opacity: 1, scale: 1, duration: 0.3 });
+  cursorDot.innerText = "";
+});
+
+heroSection.addEventListener("mouseenter", () => {
+  gsap.to(cursorDot, { scale: 12, duration: 0.3, ease: "power3.out" });
+  gsap.to(cursorFollower, { opacity: 0, scale: 0, duration: 0.3 });
+});
+
+heroSection.addEventListener("mouseleave", () => {
+  gsap.to(cursorDot, { scale: 1, duration: 0.3, ease: "power3.out" });
+  gsap.to(cursorFollower, { opacity: 1, scale: 1, duration: 0.3 });
 });
 
 function magneticButton(element) {
@@ -205,53 +291,35 @@ const a = document.getElementById("themeBtn");
 magneticButton(a);
 
 document.getElementById("themeBtn").addEventListener("mouseover", () => {
-  cursorFollower.style.opacity = 0;
-
-  console.log(themeProvider.className.split(" ").includes("dark"));
   themeProvider.className.split(" ").includes("dark")
-    ? (cursorFollowerDot.innerText = "Light Mode")
-    : (cursorFollowerDot.innerText = "Dark Mode");
-  cursorFollowerDot.style.transform = "scale(10)";
-});
-document.getElementById("themeBtn").addEventListener("mouseleave", () => {
-  cursorFollower.style.opacity = 1;
-  cursorFollowerDot.innerText = "";
-  cursorFollowerDot.style.transform = "scale(1)";
-});
-
-document.getElementById("heroContent").addEventListener("mouseover", () => {
-  cursorFollower.style.opacity = 0;
-  cursorFollowerDot.style.transform = "scale(10)";
-});
-document.getElementById("heroContent").addEventListener("mouseleave", () => {
-  cursorFollower.style.opacity = 1;
-  cursorFollowerDot.style.transform = "scale(1)";
+    ? (cursorDot.innerText = "Light Mode")
+    : (cursorDot.innerText = "Dark Mode");
 });
 
 document.querySelectorAll(".navbar span").forEach((button) => {
   button.addEventListener("mouseover", () => {
-    cursorFollower.style.opacity = 0;
-    cursorFollowerDot.innerText = "View";
-    cursorFollowerDot.style.transform = "scale(10)";
+    gsap.to(cursorDot, { scale: 12, duration: 0.3, ease: "power3.out" });
+    gsap.to(cursorFollower, { scale: 0, opacity: 0, duration: 0.3 });
+    cursorDot.innerText = "View";
   });
   button.addEventListener("mouseleave", () => {
-    cursorFollower.style.opacity = 1;
-    cursorFollowerDot.innerText = "";
-    cursorFollowerDot.style.transform = "scale(1)";
+    cursorDot.innerText = "";
+    gsap.to(cursorDot, { scale: 1, duration: 0.3, ease: "power3.out" });
+    gsap.to(cursorFollower, { scale: 1, opacity: 1, duration: 0.3 });
   });
 });
 
 const svg = `<svg class="arrow-link" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><polygon fill-rule="evenodd" points="19 6.414 5.707 19.707 4.293 18.293 17.586 5 10 5 10 3 21 3 21 14 19 14"/></svg>`;
 
 document.querySelectorAll(".footer-social span").forEach((button) => {
-  button.addEventListener("mouseover", () => {
-    cursorFollower.style.opacity = 0;
-    cursorFollowerDot.innerHTML = svg;
-    cursorFollowerDot.style.transform = "scale(10)";
+  button.addEventListener("mouseenter", () => {
+    cursorDot.innerHTML = svg;
+    gsap.to(cursorDot, { scale: 12, duration: 0.3, ease: "power3.out" });
+    gsap.to(cursorFollower, { opacity: 0, scale: 0, duration: 0.3 });
   });
   button.addEventListener("mouseleave", () => {
-    cursorFollower.style.opacity = 1;
-    cursorFollowerDot.innerHTML = "";
-    cursorFollowerDot.style.transform = "scale(1)";
+    cursorDot.innerHTML = "";
+    gsap.to(cursorDot, { scale: 1, duration: 0.3, ease: "power3.out" });
+    gsap.to(cursorFollower, { opacity: 1, scale: 1, duration: 0.3 });
   });
 });
